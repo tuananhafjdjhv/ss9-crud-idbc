1c751491e91ccbbd42166aef2a99d6f2f57fa688
tuan_anh <119550348+tuananhafjdjhv@users.noreply.github.com>
2023-05-07 15:26:31 +0700

    hihi

--- src/main/java/service/UserServiceIMPL.java
+++ src/main/java/service/UserServiceIMPL.java
@@ -18,7 +18,7 @@ public class UserServiceIMPL implements IUserService{
     private static final String SELECT_USER_BY_ID = "select id,name,email,country from users where id =?";
     private static final String SELECT_ALL_USERS = "select * from users";
     private static final String DELETE_USERS_SQL = "delete from users where id = ?;";
-    private static final String UPDATE_USERS_SQL = "update user set name = ?,email= ?, country =? where id = ?;";
+    private static final String UPDATE_USERS_SQL = "update users set name = ?,email= ?, country =? where id = ?;";
 
     public UserServiceIMPL() {
     }
